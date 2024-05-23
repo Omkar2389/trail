0d1ee51e4881bf16d2380fed35a144fd0e33160c
Omkar2389 <omkarbelhekar2389@gmail.com>
2024-05-23 11:21:25 +0530

    commit

---
+++ assets/project/clone project/script.js
@@ -0,0 +1,9 @@
+function dropdown() {
+    var dropdownMenu = document.getElementById("mydp");
+    if (dropdownMenu.style.display === "none") {
+      dropdownMenu.style.display = "block";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+  }
+  
